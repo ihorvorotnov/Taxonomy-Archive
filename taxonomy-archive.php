49e090387d8d00ab03d987d19c93dba1e862e6f1
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2015-09-09 23:41:18 +0300

    Introduce main plugin file

---
+++ taxonomy-archive.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Taxonomy Archive
+ * Plugin URI:  http://ihorvorotnov.com/plugins/taxonomy-archive/
+ * Description: Add support for top-level taxonomy archive pages, just like posts and custom post types have.
+ * Version:     0.1.0
+ * Author:      Ihor Vorotnov <ihor.vorotnov@gmail.com>
+ * Author URI:  http://ihorvorotnov.com/
+ * License:     GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: taxonomy-archive
+ * Domain Path: /languages/
+ *
+ * @package Taxonomy_Archive
+ */
+
